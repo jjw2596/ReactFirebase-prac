51e07eb9d09ca701b57b605bdcac051e2486de60
jjw2596 <jjw2596@naver.com>
2022-08-01 03:28:34 +0900

    Firebase DB add

--- wordcloud/src/components/App.js
+++ wordcloud/src/components/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import {HashRouter as Router, Route, Routes} from 'react-router-dom';
 import AppShell from './AppShell';
+import Home from './Home';
+import Texts from './Texts';
+import Words from './Words';
 
 class App extends React.Component {
     render() {
         return (
-            <AppShell/>
+            <Router>
+                <AppShell>
+                    <div>
+                        <Routes>
+                            <Route exact path="/" element={<Home/>}/>
+                            <Route exact path="/texts" element={<Texts/>}/>
+                            <Route exact path="/words" element={<Words/>}/>
+                        </Routes>
+                    </div>
+                </AppShell>
+            </Router>
         );
     }
 }
